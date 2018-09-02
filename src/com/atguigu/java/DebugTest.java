075d9464fbfb4394b727fc832f5c551d89fac6c3
xiaocan <xiaocan817@163.com>
2018-09-02 08:35:44 +0800

    commit DebugTest

---
+++ src/com/atguigu/java/DebugTest.java
@@ -0,0 +1,31 @@
+package com.atguigu.java;
+
+import java.util.HashMap;
+
+/**
+ * @author XC
+ * @date 2018/9/1 - 20:50
+ */
+public class DebugTest {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name","Tom");
+        map.put("age","12");
+        map.put("school","Tsinghua");
+        map.put("major","computer");
+
+        String age = map.get("age");
+        System.out.println("age = " + age);
+
+        map.remove("major");
+        System.out.println(map);
+    }
+
+
+}
